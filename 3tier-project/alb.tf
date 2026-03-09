3f868eac2245417a27021baa9addba6e9f6b4dcc
Vijay Kumbhar <kumbharvijay759@gmail.com>
2026-03-09 08:21:54 +0530

    Create ALB, target group, and listener resources
    Added resources for target group, Application Load Balancer, and listener.

--- 3tier-project/alb.tf
+++ 3tier-project/alb.tf
@@ -1,1 +1,54 @@
+#create target group
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "App-target-group"
+  }
+}
+
+
+#created Application Load Balanceer
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  load_balancer_type = "application"
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  security_groups = [aws_security_group.alb_sg.id]
+
+  tags = {
+    Name = "app-alb"
+  }
+}
+
+
+#created listener
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+
 
